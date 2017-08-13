97abb4c3dc68bd8a73f47156d5e2f00b98de7904
Diego Krulewietki <diegokrule@hotmail.com>
2017-08-13 03:22:47 +0200

    initial commit

---
+++ src/main/java/com/nixsystem/mc/plugin/worldProtect/WPlugin.java
@@ -0,0 +1,17 @@
+package com.nixsystem.mc.plugin.worldProtect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("onEnable has been invoked!!");
+	}
+
+	@Override
+	public void onDisable() {
+		getLogger().info("onEnable has been invoked!!");
+	}
+
+}
